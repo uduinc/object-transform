b4d2585935df2d2ef9cafc13dc776369a09602c4
Geoffrey Corey <geoff@geoffcorey.com>
2015-08-04 11:36:54 -0400

    test for group translation and type conversion

--- test/group-test.js
+++ test/group-test.js
@@ -13,20 +13,72 @@ describe('group rules', function()
 	};
 	describe('map new group', function()
 	{
-		it('code1 and txt1 should be in zzz', function(done)
+		it('code1 and txt1 should be in aaa', function(done)
 		{
-			OT.mapGroup({'code1':'code','txt1':'txt'},'zzz');
+			OT.mapGroup(
+			{
+				'code1': 'code',
+				'txt1': 'txt'
+			}, 'aaa');
 			var output = OT.transform(input);
-			should.exist(output.zzz);
-			should.exist(output.zzz[0].code);
-			should.exist(output.zzz[0].txt);
-			output.zzz[0].code.should.be.eql(1);
-			output.zzz[0].txt.should.be.eql('desc');
+			should.exist(output.aaa);
+			should.exist(output.aaa[0].code);
+			should.exist(output.aaa[0].txt);
+			output.aaa[0].code.should.be.eql(1);
+			output.aaa[0].txt.should.be.eql('desc');
+			done();
+		});
+		it('code2 should be in bbb and type int, but txt2 not exist', function(done)
+		{
+			OT.mapGroup(
+			{
+				'code2': {name:'code',type:'int'},
+				'txt2':
+				{
+					name: 'txt',
+					translate:
+					{
+						'.': ''
+					}
+				}
+			}, 'bbb');
+			var output = OT.transform(input);
+			should.exist(output.bbb);
+			should.exist(output.bbb[0].code);
+			should.not.exist(output.bbb[0].txt);
+			output.bbb[0].code.should.be.eql(10);
+			done();
+		});
+		it('txt1 should be in ccc and value changed', function(done)
+		{
+			OT.mapGroup(
+				{
+					'code1': 'code',
+					'txt1':
+					{
+						name: 'txt',
+						translate:
+						{
+							'desc': 'new desc'
+						}
+					}
+				},
+				'ccc');
+			var output = OT.transform(input);
+			should.exist(output.ccc);
+			should.exist(output.ccc[0].code);
+			should.exist(output.ccc[0].txt);
+			output.ccc[0].code.should.be.eql(1);
+			output.ccc[0].txt.should.be.eql('new desc');
 			done();
 		});
 		it('txt3 requires code3 to exist', function(done)
 		{
-			OT.mapGroup({'code3':'code','txt3':'txt'},'xxx', 'code3');
+			OT.mapGroup(
+			{
+				'code3': 'code',
+				'txt3': 'txt'
+			}, 'xxx', 'code3');
 			var output = OT.transform(input);
 			should.not.exist(output.xxx);
 			done();
